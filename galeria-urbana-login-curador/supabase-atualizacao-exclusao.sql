89e15e4902e566d5aad4ecbcb35f6b1dc29f34f0
ibrandao50-pixel <ibrandao50@gmail.com>
2026-07-22 19:18:55 -0300

    Delete galeria-urbana-login-curador directory

--- galeria-urbana-login-curador/supabase-atualizacao-exclusao.sql
+++
@@ -1,8 +0,0 @@
--- Execute uma vez no Supabase: SQL Editor > New query > Run
--- Permite que curadores autenticados removam do Storage a fotografia de uma obra excluída.
-drop policy if exists "Curadores excluem imagens" on storage.objects;
-create policy "Curadores excluem imagens"
-on storage.objects
-for delete
-to authenticated
-using (bucket_id = 'imagens-obras');
